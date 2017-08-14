225b72b40191269faebd92bc9bb0c045ad145ada
sks4321 <sks5624@naver.com>
2017-08-14 12:17:40 +0900

    입찰/낙찰/결제 Vo 작성

--- ROOT/src/main/java/com/kpsl/auction/bid/vo/BidVo.java
+++ ROOT/src/main/java/com/kpsl/auction/bid/vo/BidVo.java
@@ -1,5 +1,65 @@
 package com.kpsl.auction.bid.vo;
 
 public class BidVo {
+	private String bidCode;
+	private String userBuyerId;
+	private	String auctionGoodsCode;
+	private String userSellerId;
+	private int bidPrice;
+	private String bidDate;
+	
+	public String getBidCode() {
+		return bidCode;
+	}
 
+	public void setBidCode(String bidCode) {
+		this.bidCode = bidCode;
+	}
+
+	public String getUserBuyerId() {
+		return userBuyerId;
+	}
+
+	public void setUserBuyerId(String userBuyerId) {
+		this.userBuyerId = userBuyerId;
+	}
+
+	public String getAuctionGoodsCode() {
+		return auctionGoodsCode;
+	}
+
+	public void setAuctionGoodsCode(String auctionGoodsCode) {
+		this.auctionGoodsCode = auctionGoodsCode;
+	}
+
+	public String getUserSellerID() {
+		return userSellerId;
+	}
+
+	public void setUserSellerID(String userSellerID) {
+		this.userSellerId = userSellerID;
+	}
+
+	public int getBidPrice() {
+		return bidPrice;
+	}
+
+	public void setBidPrice(int bidPrice) {
+		this.bidPrice = bidPrice;
+	}
+
+	public String getBidDate() {
+		return bidDate;
+	}
+
+	public void setBidDate(String bidDate) {
+		this.bidDate = bidDate;
+	}
+
+	@Override
+	public String toString() {
+		return "BidVo [bidCode=" + bidCode + ", userBuyerId=" + userBuyerId + ", auctionGoodsCode=" + auctionGoodsCode
+				+ ", userSellerID=" + userSellerId + ", bidPrice=" + bidPrice + ", bidDate=" + bidDate + "]";
+	}
+	
 }
--- ROOT/src/main/java/com/kpsl/auction/goodspayment/vo/GoodsPaymentVo.java
+++ ROOT/src/main/java/com/kpsl/auction/goodspayment/vo/GoodsPaymentVo.java
@@ -2,4 +2,76 @@ package com.kpsl.auction.goodspayment.vo;
 
 public class GoodsPaymentVo {
 
+	private String goodsPaymentCode;
+	private String successfulBidCode;
+	private String goodsPaymentDate;
+	private int goodsPaymentPrice;
+	private int goodsPaymentFees;
+	private String goodsPaymentState;
+	private String goodsPaymentRemarks;
+	
+	public String getGoodsPaymentCode() {
+		return goodsPaymentCode;
+	}
+
+	public void setGoodsPaymentCode(String goodsPaymentCode) {
+		this.goodsPaymentCode = goodsPaymentCode;
+	}
+
+	public String getSuccessfulBidCode() {
+		return successfulBidCode;
+	}
+
+	public void setSuccessfulBidCode(String successfulBidCode) {
+		this.successfulBidCode = successfulBidCode;
+	}
+
+	public String getGoodsPaymentDate() {
+		return goodsPaymentDate;
+	}
+
+	public void setGoodsPaymentDate(String goodsPaymentDate) {
+		this.goodsPaymentDate = goodsPaymentDate;
+	}
+
+	public int getGoodsPaymentPrice() {
+		return goodsPaymentPrice;
+	}
+
+	public void setGoodsPaymentPrice(int goodsPaymentPrice) {
+		this.goodsPaymentPrice = goodsPaymentPrice;
+	}
+
+	public int getGoodsPaymentFees() {
+		return goodsPaymentFees;
+	}
+
+	public void setGoodsPaymentFees(int goodsPaymentFees) {
+		this.goodsPaymentFees = goodsPaymentFees;
+	}
+
+	public String getGoodsPaymentState() {
+		return goodsPaymentState;
+	}
+
+	public void setGoodsPaymentState(String goodsPaymentState) {
+		this.goodsPaymentState = goodsPaymentState;
+	}
+
+	public String getGoodsPaymentRemarks() {
+		return goodsPaymentRemarks;
+	}
+
+	public void setGoodsPaymentRemarks(String goodsPaymentRemarks) {
+		this.goodsPaymentRemarks = goodsPaymentRemarks;
+	}
+
+	@Override
+	public String toString() {
+		return "GoodsPaymentVo [goodsPaymentCode=" + goodsPaymentCode + ", successfulBidCode=" + successfulBidCode
+				+ ", goodsPaymentDate=" + goodsPaymentDate + ", goodsPaymentPrice=" + goodsPaymentPrice
+				+ ", goodsPaymentFees=" + goodsPaymentFees + ", goodsPaymentState=" + goodsPaymentState
+				+ ", goodsPaymentRemarks=" + goodsPaymentRemarks + "]";
+	}
+	
 }
