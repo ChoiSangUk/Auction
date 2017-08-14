c44f2a6ba68a762491a68f42a2d4f06e60d4ffba
JuhoPark <wngh7247@naver.com>
2017-08-14 10:08:02 +0900

    광고관리(ad) 매퍼파일 추가/광고단가vo 변수 선언

--- ROOT/src/main/java/com/kpsl/auction/ad/vo/AdUnitPriceVo.java
+++ ROOT/src/main/java/com/kpsl/auction/ad/vo/AdUnitPriceVo.java
@@ -1,5 +1,43 @@
 package com.kpsl.auction.ad.vo;
-
+/****************************
+ * 광고 단가관리vo
+ * 
+ ****************************/
 public class AdUnitPriceVo {
-
+	
+	private String adUnitPirceCode;
+	private String adUnitPirceName;
+	private String adUnitPricePrice;
+	private String adUnitPriceState;
+	
+	public String getAdUnitPirceCode() {
+		return adUnitPirceCode;
+	}
+	public void setAdUnitPirceCode(String adUnitPirceCode) {
+		this.adUnitPirceCode = adUnitPirceCode;
+	}
+	public String getAdUnitPirceName() {
+		return adUnitPirceName;
+	}
+	public void setAdUnitPirceName(String adUnitPirceName) {
+		this.adUnitPirceName = adUnitPirceName;
+	}
+	public String getAdUnitPricePrice() {
+		return adUnitPricePrice;
+	}
+	public void setAdUnitPricePrice(String adUnitPricePrice) {
+		this.adUnitPricePrice = adUnitPricePrice;
+	}
+	public String getAdUnitPriceState() {
+		return adUnitPriceState;
+	}
+	public void setAdUnitPriceState(String adUnitPriceState) {
+		this.adUnitPriceState = adUnitPriceState;
+	}
+	
+	@Override
+	public String toString() {
+		return "AdUnitPriceVo [adUnitPirceCode=" + adUnitPirceCode + ", adUnitPirceName=" + adUnitPirceName
+				+ ", adUnitPricePrice=" + adUnitPricePrice + ", adUnitPriceState=" + adUnitPriceState + "]";
+	}
 }
