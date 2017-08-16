0f22e8c51098081a0c0168d1efc8c2727dffa9c3
kang6047 <rkdalstn6047@naver.com>
2017-08-16 10:16:22 +0900

    mybatis 연동 테스트(성공)

--- ROOT/src/main/java/com/kpsl/auction/auctiongoods/controller/AuctionGoodsController.java
+++ ROOT/src/main/java/com/kpsl/auction/auctiongoods/controller/AuctionGoodsController.java
@@ -2,6 +2,7 @@ package com.kpsl.auction.auctiongoods.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.kpsl.auction.auctiongoods.service.AuctionGoodsService;
 import com.kpsl.auction.auctiongoods.vo.AuctionGoodsVo;
 
+@Controller
 public class AuctionGoodsController {
 	@Autowired
-	AuctionGoodsService auctionGoodsService;
+	private AuctionGoodsService auctionGoodsService;
 	
 	@RequestMapping(value = "/auctiongoods/selectauctiongoods", method = RequestMethod.GET)
 		public String selectAuctionGoods(Model model) {
