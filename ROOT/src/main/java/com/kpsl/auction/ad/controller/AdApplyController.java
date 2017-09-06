da7f114ca747c8c2cf95b11f71fa384aa92bfde3
JuhoPark <wngh7247@naver.com>
2017-09-06 16:43:31 +0900

    pom.xml test

--- ROOT/src/main/java/com/kpsl/auction/ad/controller/AdApplyController.java
+++ ROOT/src/main/java/com/kpsl/auction/ad/controller/AdApplyController.java
@@ -2,10 +2,8 @@ package com.kpsl.auction.ad.controller;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
-//import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +15,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.kpsl.auction.ad.service.AdApplyService;
 import com.kpsl.auction.ad.service.AdPaymentService;
